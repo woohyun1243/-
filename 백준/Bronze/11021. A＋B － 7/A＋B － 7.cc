00b7cb9f96733ccd34b93832a580b09be68fef76
woohyun1243 <jungchh14@gmail.com>
2026-03-23 22:07:18 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int t{}, a{}, b{};
+	std::cin >> t;
+
+	for (int i{ 1 }; i <= t; i++) {
+		std::cin >> a >> b;
+		std::cout << "Case #" << i << ": " << a + b << "\n";
+	}
+}
